4335361983454d5274c0474de3905b7d61958a12
Changyuan Lin <linchangyuan1996@gmail.com>
2020-11-05 14:38:34 -0500

    Sample code

---
+++ src/eecs2011/project/Solution.java
@@ -0,0 +1,51 @@
+package eecs2011.project;
+
+/**
+ * The Solution class is where you write your solutions.
+ * Please do NOT modify the method declarations and the package name.
+ * You can add other classes to the package if needed.
+ * Read data from standard input and write your answer to standard output.
+ * We use pipelines to send input to your program and send your output to our judge system.
+ * Please do NOT read and write data from and to the disk.
+ * You should strictly follow the sample output format.
+ * Please do not use any built-in/third-party data structures or algorithms.
+ * Memory Limit: 256MB  Time Limit: 10s
+ */
+public class Solution {
+
+    /**
+     * Solution to Part 1
+     */
+    public void check_validity() {
+        ; // read from standard input
+        ; // do something
+        System.out.println("True"); // write your answer to standard output
+    }
+
+    /**
+     * Solution Part 2
+     */
+    public void schedule_1() {
+        ; // read from standard input
+        ; // do something
+        ; // write your answer to standard output
+    }
+
+    /**
+     * Solution to Part 3
+     */
+    public void schedule_x() {
+        ; // read from standard input
+        ; // do something
+        ; // write your answer to standard output
+    }
+
+    /**
+     * Solution to Part 4 (optional)
+     */
+    public void schedule_2() {
+        ; // read from standard input
+        ; // do something
+        ; // write your answer to standard output
+    }
+}
